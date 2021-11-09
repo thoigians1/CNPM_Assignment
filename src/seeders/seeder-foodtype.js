250ef77b1fdee00ba8e95771664d98b76d2e09f5
Dang Tu <tu07122001@gmail.com>
2021-11-09 21:47:56 +0700

    tu

--- src/seeders/seeder-foodtype.js
+++ src/seeders/seeder-foodtype.js
@@ -4,25 +4,40 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Foodtypes', [
             {
-                name: 'Gà rán',
+                name: 'Cơm',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
-                name: 'Cơm chiên',
+                name: 'Canh',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
-                name: 'Gà chiên',
+                name: 'Cháo',
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
-                name: 'Cơm trộn',
+                name: 'Ăn kèm',
                 createdAt: new Date(),
                 updatedAt: new Date()
-            }
+            },
+            {
+                name: 'Hải sản',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Món gà và chim',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Đồ uống',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
         ]);
     },
 
